e2296757e2b251338bca30d65985d736450f3ced
Stephen Peters <7speter@gmail.com>
2019-09-02 09:00:28 -0400

    Changes navbar color again

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,14 +3,11 @@ import AnchorLink from 'react-anchor-link-smooth-scroll';
 import styled from "styled-components";
  
 const StyledNav = styled.nav`
-    background: #03001e; /* fallback for old browsers */
-    background: -webkit-linear-gradient(to right, #03001e, #7303c0, #de3021, #f7b733); /* Chrome 10-25, Safari 5.1-6 */
-    background: linear-gradient(to right, #03001e, #7303c0, #de3021, #f7b733); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-    /*Gradient from https://uigradients.com/. Was based on argon*/
+    
     position: fixed;
     right:0;
     left: 0;
-    background-color: rgb(6, 243, 184, .2);
+    background-color: rgb(40,40,40);
     top: 0;
     height: 3rem;
     width: 100vw;
