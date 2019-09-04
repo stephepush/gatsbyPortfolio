dc0a8a0b122f0ca2c128010cccdfd8e75df6e51b
Stephen Peters <7speter@gmail.com>
2019-09-04 15:19:51 -0400

    Adds prelim styling to navbar

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,25 +3,43 @@ import AnchorLink from 'react-anchor-link-smooth-scroll';
 import styled from "styled-components";
  
 const StyledNav = styled.nav`
-    
+    display: grid;
+    grid-template-columns: 2.5% 10% minmax(10px, 1700px) 25% 2.5%;
     position: fixed;
     right:0;
     left: 0;
-    background-color: rgb(40,40,40);
+    background-color: rgb(62,65,44);
     top: 0;
     height: 3rem;
-    width: 100vw;
+    max-width: 100%;
     z-index: 999;
-    color: rgba(245,245,245,1);
+    color: rgba(255,201,0,1);
+`;
+
+const SectionLinkContainer = styled.div`
+    grid-column: 4/5;
+    
+    align-self: center;
+    display: flex;
+    flex-wrap: nowrap;
+    justify-content: space-between;
+`;
+
+const StyledAnchorLink = styled(AnchorLink)`
+    color: rgba(255,201,0,1);
+    
 `;
 
 const Nav = () => (
     <StyledNav>
         {/* Todo: Change the text styling in the anchorlink elems */}
-            <AnchorLink offset='100' href='#home'>Home</AnchorLink>
-            <AnchorLink href='#about'>About</AnchorLink>
-            <AnchorLink href='#projects'>Projects</AnchorLink>
-            <AnchorLink href='#contact'>Contact</AnchorLink>
+        <SectionLinkContainer>
+            
+            <StyledAnchorLink href='#about'>About</StyledAnchorLink>
+            <StyledAnchorLink href='#projects'>Projects</StyledAnchorLink>
+            <StyledAnchorLink href='#contact'>Contact</StyledAnchorLink>
+        </SectionLinkContainer>
+           
         
     </StyledNav> 
 
