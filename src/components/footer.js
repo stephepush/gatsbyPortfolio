351b5c3b4305765d2d8b979ccc5d5fb4bf0e0261
Stephen Peters <7speter@gmail.com>
2019-09-04 00:33:54 -0400

    Adds content to footer component and adds footer to project

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import styled from "styled-components";
+
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { faHandSpock } from "@fortawesome/free-solid-svg-icons";
 
 const Footer = () => {
     return(
         <footer>
-            Created by Stephen Peters with GatsbyJS in 2019
+            <p>Initially created by Stephen Peters w GatsbyJS in 2019</p>
+            <p>LLAP <FontAwesomeIcon icon={ faHandSpock } size="s"/></p>
         </footer>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,15 @@ import React from "react"
 import { Link } from "gatsby"
 
 import 'normalize.css';
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-import Home from "../components/home"
-import About from "../components/about"
-import Projects from "../components/projects"
-import Contact from "../components/contact"
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
+import Home from "../components/home";
+import About from "../components/about";
+import Projects from "../components/projects";
+import Contact from "../components/contact";
 import Nav from "../components/nav";
+import Footer from "../components/footer";
 
 const IndexPage = () => {
   {/* <Layout>
@@ -29,6 +30,7 @@ const IndexPage = () => {
       <About />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   )
 }
